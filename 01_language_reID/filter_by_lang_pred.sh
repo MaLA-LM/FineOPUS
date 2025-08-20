2a264a165b3ef5fa264e2dd0a81fd3bf0cdba461
zihao <zihao-lee@outlook.com>
2025-08-20 18:31:33 +0300

    Refactor language classification and update execution counts in Jupyter notebook. Change job name in filter script for ConLID processing. Update JSON structure for language source classification to use tier names.

--- 01_language_reID/filter_by_lang_pred.sh
+++ 01_language_reID/filter_by_lang_pred.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=mala-opus-filter-glotlid
+#SBATCH --job-name=mala-opus-filter-conlid
 #SBATCH --output=../logs/%x_%j.out
 #SBATCH --error=../logs/%x_%j.err
 #SBATCH --partition=small
@@ -21,10 +21,13 @@ source /flash/project_462000941/venv/opus2410_env/bin/activate
 NUM_PROC=16
 CONF_THRESHOLD=0.9
 
-SOURCE_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-GlotLID"
-OUTPUT_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-GlotLID-Threshold-${CONF_THRESHOLD//./_}"
+# SOURCE_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-GlotLID"
+# OUTPUT_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-GlotLID-Threshold-${CONF_THRESHOLD//./_}"
+# FILELIST="./mala-opus-dedup-2410-ReLID-by-GlotLID-filelists/filelist_${SLURM_ARRAY_TASK_ID}.txt"
 
-FILELIST="./mala-opus-dedup-2410-ReLID-by-GlotLID-filelists/filelist_${SLURM_ARRAY_TASK_ID}.txt"
+# SOURCE_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-ConLID"
+# OUTPUT_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-ConLID-Threshold-${CONF_THRESHOLD//./_}"
+# FILELIST="./mala-opus-dedup-2410-ReLID-by-ConLID-filelists/filelist_${SLURM_ARRAY_TASK_ID}.txt"
 
 python ./filter_by_lang_pred.py \
   --source_dir "$SOURCE_DIR" \
