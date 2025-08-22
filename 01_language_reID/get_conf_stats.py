682dd4901ae9ee1782c5a0ed62123f427c3b0a0b
zihao <zihao-lee@outlook.com>
2025-08-22 22:05:50 +0300

    Add scripts for generating language confidence statistics

---
+++ 01_language_reID/get_conf_stats.py
@@ -0,0 +1,79 @@
+import os
+from glob import glob
+import json
+from datasets import load_dataset
+from collections import defaultdict
+import logging
+import sys
+import argparse
+import gzip
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source_dir", required=True, help="Directory containing .jsonl files with predictions")
+    parser.add_argument("--output_dir", required=True, help="Output directory")
+    parser.add_argument("--filelist", type=str, help="Optional: Path to file containing list of files to process")
+    parser.add_argument("--job_id", type=int, help="Optional: Job ID")
+    args = parser.parse_args()
+
+    logging.info("Arguments:")
+    logging.info(f"  Source Directory: {args.source_dir}")
+    logging.info(f"  Output Directory: {args.output_dir}")
+
+    if args.filelist:
+        with open(args.filelist, encoding="utf-8") as f:
+            all_files = [line.strip() for line in f if line.strip()]
+    else:
+        all_files = sorted(glob(f"{args.source_dir}/**/*.jsonl", recursive=True))
+
+    if not all_files:
+        logging.warning("No files found to process.")
+        exit(1)
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    lang_conf = defaultdict(list)
+    total_files = len(all_files)
+
+    for idx, input_path in enumerate(all_files, 1):
+        logging.info(f"[{idx}/{total_files}] Processing file: {os.path.basename(input_path)}")
+
+        try:
+            ds = load_dataset("json", data_files=input_path, split="train").select_columns(
+                ["source_lang", "source_predlang_conf", "target_lang", "target_predlang_conf"]
+            )
+            
+            src_langs = ds["source_lang"]
+            src_confs = ds["source_predlang_conf"]
+            tgt_langs = ds["target_lang"]
+            tgt_confs = ds["target_predlang_conf"]
+
+            for lang, conf in zip(src_langs, src_confs):
+                if lang is not None:
+                    lang_conf[lang].append(conf)
+
+            for lang, conf in zip(tgt_langs, tgt_confs):
+                if lang is not None:
+                    lang_conf[lang].append(conf)
+        except Exception as e:
+            logging.error(f"Error processing file {input_path}: {e}")
+            continue
+
+    if args.job_id:
+        stats_filename = f"conf_stats_{args.job_id}.json.gz"
+    else:
+        stats_filename = "conf_stats.json.gz"
+
+    output_file = os.path.join(args.output_dir, stats_filename)
+
+    with gzip.open(output_file, "wt", encoding="utf-8") as f:
+        json.dump(lang_conf, f, ensure_ascii=False)
+
+    logging.info(f"Stats saved to {output_file}")
---
+++ 01_language_reID/get_conf_stats.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=mala-opus-conf-stats-glotlid
+#SBATCH --output=../logs/%x_%j.out
+#SBATCH --error=../logs/%x_%j.err
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=32G
+#SBATCH --account=project_462000675
+#SBATCH --array=0-127
+
+start_time=$(date +%s)
+echo "Job started at: $(date)"
+
+module use /appl/local/csc/modulefiles/
+module load pytorch/2.5
+source /flash/project_462000941/venv/opus2410_env/bin/activate
+
+export HF_HOME="/scratch/project_462000964/cache/huggingface"
+
+SOURCE_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-GlotLID"
+OUTPUT_DIR="./mala-opus-dedup-2410-ReLID-by-GlotLID-conf-stats"
+FILELIST="./mala-opus-dedup-2410-ReLID-by-GlotLID-filelists/filelist_${SLURM_ARRAY_TASK_ID}.txt"
+
+# SOURCE_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-ConLID"
+# OUTPUT_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-ConLID-Threshold-${CONF_THRESHOLD//./_}"
+# FILELIST="./mala-opus-dedup-2410-ReLID-by-ConLID-filelists/filelist_${SLURM_ARRAY_TASK_ID}.txt"
+
+python ./get_conf_stats.py \
+  --source_dir "$SOURCE_DIR" \
+  --output_dir "$OUTPUT_DIR" \
+  --filelist "$FILELIST" \
+  --job_id "${SLURM_ARRAY_TASK_ID}"
+
+end_time=$(date +%s)
+echo "Job ended at: $(date)"
+
+duration=$((end_time - start_time))
+echo "Job duration: $(date -u -d @${duration} +%T)"
