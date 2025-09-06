f31ac8cd2f3ab152609f71d14e1c38ab8c256ffd
zihao <zihao-lee@outlook.com>
2025-09-06 23:20:57 +0300

    Update .gitignore to include new ENSEMBLED output directory; modify ensemble script to adjust resource allocations and output paths.

--- 01_language_reID/ensemble_relid.sh
+++ 01_language_reID/ensemble_relid.sh
@@ -5,11 +5,11 @@
 #SBATCH --partition=small
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=3-00:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1-00:00:00
 #SBATCH --mem=128G
-#SBATCH --account=project_462000964
-#SBATCH --array=0-63
+#SBATCH --account=project_462000941
+#SBATCH --array=1-127
 
 start_time=$(date +%s)
 echo "Job started at: $(date)"
@@ -24,7 +24,7 @@ GLOTLID_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-Gl
 CONLID_DIR="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-by-ConLID"
 GLOTLID_THR_JSON="./mala-opus-dedup-2410-ReLID-by-GlotLID-conf-stats/aggregated_language_confidence_stats_quantiles.json"
 CONLID_THR_JSON="./mala-opus-dedup-2410-ReLID-by-ConLID-conf-stats/aggregated_language_confidence_stats_quantiles.json"
-OUT_ROOT="/scratch/project_462000964/MaLA-LM/mala-opus-dedup-2410-ReLID-ENSEMBLED"
+OUT_ROOT="/scratch/project_462000941/members/zihao/OPUS2410/01_language_reID/mala-opus-dedup-2410-ReLID-ENSEMBLED/tmp_${SLURM_ARRAY_TASK_ID}"
 FILELIST="./mala-opus-dedup-2410-ReLID-Relpath-filelists/filelist_${SLURM_ARRAY_TASK_ID}.txt"
 
 
@@ -34,10 +34,11 @@ python ./ensemble_relid.py \
   --glotlid_thr_json "$GLOTLID_THR_JSON" \
   --conlid_thr_json "$CONLID_THR_JSON" \
   --out_root "$OUT_ROOT" \
-  --part_size 15000000 \
+  --part_size 1000000 \
   --filelist "$FILELIST" \
   --compression snappy \
-  --strict_check
+  # --strict_check
+  
 
 end_time=$(date +%s)
 echo "Job ended at: $(date)"
