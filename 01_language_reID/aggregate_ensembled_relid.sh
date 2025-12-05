3b8b22e90e7f80551a3191fb70a92d86d3e538d4
zihao <zihao-lee@outlook.com>
2025-12-05 15:56:14 +0200

    feat: update SLURM script

--- 01_language_reID/aggregate_ensembled_relid.sh
+++ 01_language_reID/aggregate_ensembled_relid.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-#SBATCH --job-name=aggregate_ensembled_relid
-#SBATCH --output=../logs/%x_%j.out
-#SBATCH --error=../logs/%x_%j.err
+#SBATCH --job-name=fineopus-aggregate-ensembled-relid
+#SBATCH --output=../logs/fineopus-aggregate-ensembled-relid/%x_%j.out
+#SBATCH --error=../logs/fineopus-aggregate-ensembled-relid/%x_%j.err
 #SBATCH --partition=small
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0-12:00:00
-#SBATCH --mem=64G
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=128G
 #SBATCH --account=project_462000964
 
 start_time=$(date +%s)
 echo "Job started at: $(date)"
 
+module purge
 module use /appl/local/csc/modulefiles/
 module load pytorch/2.5
-source /flash/project_462000941/venv/opus2410_env/bin/activate
+source ../.venv/bin/activate
 
 
-INPUT_ROOT="/scratch/project_462000941/members/zihao/OPUS2410/01_language_reID/mala-opus-dedup-2410-ReLID-ENSEMBLED-TAR-V2"
-OUTPUT_ROOT="/scratch/project_462000941/members/zihao/OPUS2410/01_language_reID/mala-opus-dedup-2410-ReLID-ENSEMBLED-FIRST-V2"
-EXTRACT_TMP_DIR="/scratch/project_462000941/members/zihao/OPUS2410/01_language_reID/mala-opus-dedup-2410-ReLID-ENSEMBLED-TMP"
+INPUT_ROOT="/scratch/project_462001069/members/zihao/FineOPUS/fineopus-original-ReLID-ENSEMBLED-TAR"
+OUTPUT_ROOT="/scratch/project_462001069/members/zihao/FineOPUS/fineopus-original-ReLID-ENSEMBLED-FIRST"
+EXTRACT_TMP_DIR="/scratch/project_462001069/members/zihao/FineOPUS/fineopus-original-ReLID-ENSEMBLED-TMP"
 
 python ./aggregate_ensembled_relid.py \
   --input_root "$INPUT_ROOT" \
