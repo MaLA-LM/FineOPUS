ae465bbedf37987d2bf4432cc0d3556c2438e987
Pinzhen Chen <pinzhen_chen@yahoo.com>
2026-06-23 21:22:10 +0100

    only create html from shuffled data; make lang2 consistent

--- 07_human_eval/annotation_samples/shuffle.py
+++ 07_human_eval/annotation_samples/shuffle.py
@@ -10,7 +10,14 @@ for lang in langs:
     for file in files:
         with open(file.format(L=lang), "r", encoding="utf-8") as f:
             for line in f:
-                data.append(json.loads(line))
+                item = json.loads(line)
+                # always make lang2_code eng_Latn
+                if item.get("lang2_code") != "eng_Latn":
+                    # swap lang1_code and lang2_code
+                    item["lang2_code"], item["lang1_code"] = item["lang1_code"], item["lang2_code"]
+                    # swap lang1_text and lang2_text
+                    item["lang2_text"], item["lang1_text"] = item["lang1_text"], item["lang2_text"]
+                data.append(item)
 
     random.shuffle(data)
     with open("shuffled_data_{L}.jsonl".format(L=lang), "w", encoding="utf-8") as f:
