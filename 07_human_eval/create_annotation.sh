5cf8fa5e1d4ab4668709fcd71a47a2b29ac020c9
Pinzhen Chen <pinzhen_chen@yahoo.com>
2026-06-23 20:57:51 +0100

    update readme/comments

--- 07_human_eval/create_annotation.sh
+++ 07_human_eval/create_annotation.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 nsamples=100
+
+# change the language codes
 lang1code="abk_Cyrl"
 lang2code="bos_Latn"
-hfpath="MaLA-LM/FineOPUS-Filtered-Stage4"
-outputdatafolder="./annotation_samples"
-outputhtmlfolder="./annotation_htmls"
 
+hfpath="MaLA-LM/FineOPUS-Filtered-Stage4"
+
+outputdatafolder="./annotation_samples"
+outputhtmlfolder="./annotation"
+
+# create annotation samples by streaming the FineOPUS-Filtered-Stage4 
+# dataset (in both forward and reverse directions) and reservoir sampling.
+# this script is generic
 python 0_create_sample_data.py \
     --hf_path ${hfpath} \
     --n_samples ${nsamples} \
@@ -14,6 +21,15 @@ python 0_create_sample_data.py \
     --lang2_code ${lang2code} \
     --output_folder ${outputdatafolder}
 
+# cross-check the FineOPUS-Original dataset (original)
+# against the FineOPUS-Filtered-Stage4 dataset (final)
+# to find unique items in FineOPUS-Original.
+# this script is heavily hard-coded
+python 0_create_sample_data_by_crosschecking.py \
+    --lang1_code ${lang1code} \
+
+# create an HTML file for human evaluation of the annotation samples.
 python 1_create_annotation_html.py \
     --input_path ${outputdatafolder} \
     --output_path ${outputhtmlfolder}
+
