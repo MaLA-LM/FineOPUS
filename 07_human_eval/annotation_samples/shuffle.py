2658954f37ad8e8d440ba5f81ff585b38bad304b
Pinzhen Chen <pinzhen_chen@yahoo.com>
2026-06-27 16:11:37 +0100

    update shuffle to skip if shuffled data exists

--- 07_human_eval/annotation_samples/shuffle.py
+++ 07_human_eval/annotation_samples/shuffle.py
@@ -1,11 +1,20 @@
+import os
+
 import json
 import random
 
-langs = ["ara_Arab", "ben_Beng", "zho_Hans", "fin_Latn"]
+random.seed(42)  # for reproducibility
+
+langs = ["fas_Arab"]
+
 # use placeholder in the file names to format them with the language code
 files = [f"sample_FineOPUS-Filtered-Stage4_{{L}}_eng_Latn_100.jsonl", f"sample_FineOPUS-Original_unique_{{L}}_eng_Latn_100.jsonl"]
 
 for lang in langs:
+    # if the file already exists, skip to the next language
+    if os.path.exists("shuffled_data_{L}.jsonl".format(L=lang)):
+        continue
+
     data = []
     for file in files:
         with open(file.format(L=lang), "r", encoding="utf-8") as f:
